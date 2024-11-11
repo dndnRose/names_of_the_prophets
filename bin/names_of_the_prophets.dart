2083bfe9dda65b3a1f821567f67f88f860648f0a
dania-lakmush <msk286.danya@gmail.com>
2024-11-11 20:49:19 +0300

    تصليحات كود

--- bin/names_of_the_prophets.dart
+++ bin/names_of_the_prophets.dart
@@ -1,26 +1,40 @@
 import 'dart:io';
 
 void main() {
-  print("ادخل اسم 1");
   String name1, name2, name3;
+
+  print("ادخل اسم 1");
   name1 = stdin.readLineSync()!;
+
   print("ادخل اسم2");
   name2 = stdin.readLineSync()!;
+
   print("ادخل اسم 3");
   name3 = stdin.readLineSync()!;
-  print('ادخل تاريخ ميلاد الشخص ١');
+
   int birthday1, birthday2, birthday3;
-  String birthdayString = stdin.readLineSync()!;
-  birthday1 = int.tryParse(birthdayString) ?? 0;
+
+  print('ادخل تاريخ ميلاد الشخص ١');
+  String birthday1String = stdin.readLineSync()!;
+  birthday1 = int.tryParse(birthday1String) ?? 0;
+
   print('ادخل تاريخ ميلاد الشخص 2');
-  birthday2 = int.tryParse(birthdayString) ?? 0;
-  birthday3 = int.tryParse(birthdayString) ?? 0;
+  String birthday2String = stdin.readLineSync()!;
+  birthday2 = int.tryParse(birthday2String) ?? 0;
+
+  print('ادخل تاريخ ميلاد الشخص 3');
+  String birthday3String = stdin.readLineSync()!;
+  birthday3 = int.tryParse(birthday3String) ?? 0;
+
   print('ادخل تاريخ وفاة 1 ');
   int death1, death2, death3;
   String deathString = stdin.readLineSync()!;
   death1 = int.tryParse(deathString) ?? 0;
+
+  String death2String = stdin.readLineSync()!;
+  death2 = int.tryParse(death2String) ?? 0;
   print('ادخل تاريخ وفاة 2 ');
-  death2 = int.tryParse(deathString) ?? 0;
-  death3 = int.tryParse(deathString) ?? 0;
+  String death3String = stdin.readLineSync()!;
+  death3 = int.tryParse(death3String) ?? 0;
   print('ادخل تاريخ وفاة 3 ');
 }
